7fa19e8cf69c42de61d992f9fae766a6f383a2e5
CDAC-pratiksha <pratikshamandlik24@gmail.com>
2023-03-07 12:39:16 +0530

    newwww

--- src/demo1/example5.java
+++ src/demo1/example5.java
@@ -1,10 +1,9 @@
 package demo1;
 
-public class example5
+public class example5 
 {
-	public static void main(String[] args)
-	{
-	System.out.println("welcome");
+	public static void main(String[] args) {
+		System.out.println("welcome prasad bhai");
 	}
 
 }
